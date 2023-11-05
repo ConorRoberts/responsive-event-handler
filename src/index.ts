d147a7732e5234e6faa4be3289ae2e3eed7e1bc5
Conor Roberts <Conor@ConorRoberts.com>
2023-11-05 16:00:35 -0500

    Initial

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+const breakpoints = ["sm", "md", "lg", "xl", "2xl"] as const;
+type Breakpoint = (typeof breakpoints)[number];
+type BreakpointHandlers<E> = Partial<Record<Breakpoint, (e: E) => any>>;
+const breakpointWidthValues: Record<Breakpoint, number> = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  "2xl": 1536,
+};
+
+export const responsiveEventHandler =
+  <E>(options: BreakpointHandlers<E>) =>
+  (event: E) => {
+    const width = window.innerWidth;
+
+    if (options["2xl"] && width >= breakpointWidthValues["2xl"]) {
+      return options["2xl"](event);
+    } else if (options.xl && width >= breakpointWidthValues.xl) {
+      return options.xl(event);
+    } else if (options.lg && width >= breakpointWidthValues.lg) {
+      return options.lg(event);
+    } else if (options.md && width >= breakpointWidthValues.md) {
+      return options.md(event);
+    } else if (options.sm && width >= breakpointWidthValues.sm) {
+      return options.sm(event);
+    }
+  };
